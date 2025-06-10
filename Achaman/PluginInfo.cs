81aec52170935f09b9fdba6eef524d94e163280f
Chaxiraxi <david.goletta@live.com>
2025-06-10 17:57:54 +0200

    Bump plugin version to 1.1.1

--- Achaman/PluginInfo.cs
+++ Achaman/PluginInfo.cs
@@ -1,9 +1,11 @@
-namespace Achaman {
-    public class PluginInfo {
+namespace Achaman
+{
+    public class PluginInfo
+    {
         public const string PLUGIN_GUID = "Achaman";
         public const string PLUGIN_NAME = "Achaman";
         public const string USERS_PLUGIN_NAME = "Achaman";
-        public const string PLUGIN_VERSION = "1.1.0";
+        public const string PLUGIN_VERSION = "1.1.1";
         public const string PLUGIN_DESCRIPTION = "Achaman is a general purpose cheat mod for Void Crew :)\nDisables progression if using it.";
         public const string PLUGIN_ORIGINAL_AUTHOR = "Chaxiraxi";
         public const string PLUGIN_AUTHORS = "Chaxiraxi";
